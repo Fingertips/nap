34753f7960991ec0ddb30086a5e76fd47671a149
Manfred Stienstra <manfred@fngtps.com>
2008-08-12 12:11:38 +0200

    Add autotest recipe. Add boolean method for response codes.

--- lib/rest/response.rb
+++ lib/rest/response.rb
@@ -1,11 +1,33 @@
 module REST
   class Response
+    CODES = [
+      [200, :ok],
+      [301, :moved_permanently],
+      [302, :found],
+      [400, :bad_request],
+      [401, :unauthorized],
+      [403, :forbidden],
+      [422, :unprocessable_entity],
+      [404, :not_found],
+      [500, :internal_server_error]
+    ]
+    
     attr_accessor :body, :headers, :status_code
     
     def initialize(status_code, headers={}, body='')
       @status_code = status_code.to_i
       @headers = headers
       @body = body
+    end
+    
+    CODES.each do |code, name|
+      define_method "#{name}?" do
+        status_code == code
+      end
+    end
+    
+    def success?
+      (status_code.to_s =~ /2../) ? true : false
     end
   end
 end
--- test/rest_response_test.rb
+++ test/rest_response_test.rb
@@ -7,4 +7,27 @@ describe "REST Response" do
       REST::Response.new(404)
     }.should.not.raise
   end
+  
+  it "should respond to all the status code names" do
+    response = REST::Response.new(200)
+    REST::Response::CODES.all? { |code, name| response.respond_to?("#{name}?") }.should == true
+  end
+  
+  it "should know if the request was a success" do
+    response = REST::Response.new(422)
+    response.success?.should == false
+    
+    response = REST::Response.new(201)
+    response.success?.should == true
+    
+    response = REST::Response.new(202)
+    response.success?.should == true
+  end
+  
+  it "should have working boolean methods for status codes" do
+    response = REST::Response.new(422)
+    response.ok?.should == false
+    response.not_found?.should == false
+    response.unprocessable_entity?.should == true
+  end
 end

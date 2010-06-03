93d2ff7e62a6f1a9f04bd35766e5cfd1181e68e3
Manfred Stienstra <manfred@fngtps.com>
2010-06-03 13:45:42 +0200

    Make client certificates actually work.

--- lib/rest/request.rb
+++ lib/rest/request.rb
@@ -16,8 +16,9 @@ module REST
     #   * <tt>tls_verify/verify_ssl</tt>: Verify the server certificate against known CA's
     #   * <tt>tls_ca_file</tt>: Use a specific file for CA certificates instead of the built-in one
     #     this only works when <tt>:tls_verify</tt> is also set.
-    #   * <tt>tls_key_file</tt>: The client certificate file to use for this request
-    #   * <tt>tls_key</tt>: The client certficate keypair to use for this request
+    #   * <tt>tls_key_and_certificate_file</tt>: The client key and certificate file to use for this request
+    #   * <tt>tls_certificate</tt>: The client certficate to use for this request
+    #   * <tt>tls_key</tt>: The client private key to use for this request
     #
     # == Examples
     #
@@ -31,6 +32,8 @@ module REST
     #     {'Accept' => 'application/json, */*', 'Content-Type' => 'application/json; charset=utf-8'}
     #   )
     #
+    # == Authentication example
+    #
     #   request = REST::Request.new(:put,
     #     URI.parse('http://example.com/pigeons/1'),
     #     {'name' => 'Homer'}.to_json,
@@ -38,13 +41,24 @@ module REST
     #     {:username => 'Admin', :password => 'secret'}
     #   )
     #
-    #   request = REST::Request.new(:get, URI.parse('http://example.com/pigeons/1'), {}, {}, {
-    #     :key => OpenSSL::PKey::RSA.new(File.read('/home/alice/keys/example.pem'))
+    # == TLS / SSL examples
+    #
+    #   # Use a client key and certificate
+    #   request = REST::Request.new(:get, URI.parse('https://example.com/pigeons/1'), {}, {}, {
+    #     :tls_key_and_certificate_file => '/home/alice/keys/example.pem'
     #   })
     #
-    #   request = REST::Request.new(:get, URI.parse('http://example.com/pigeons/1'), {}, {}, {
+    #   # Use a client certificate and key from a specific location
+    #   key_and_certificate = File.read('/home/alice/keys/example.pem')
+    #   request = REST::Request.new(:get, URI.parse('https://example.com/pigeons/1'), {}, {}, {
+    #     :tls_key => OpenSSL::PKey::RSA.new(key_and_certificate),
+    #     :tls_certificate => OpenSSL::X509::Certificate.new(key_and_certificate)
+    #   })
+    #
+    #   # Verify the server certificate against a specific certificate
+    #   request = REST::Request.new(:get, URI.parse('https://example.com/pigeons/1'), {}, {}, {
     #     :tls_verify => true,
-    #     :tls_key_file => '/home/alice/keys/example.pem'
+    #     :tls_ca_file => '/home/alice/keys/example.pem'
     #   })
     def initialize(verb, url, body=nil, headers={}, options={})
       @verb = verb
@@ -102,12 +116,17 @@ module REST
           http_request.verify_mode = OpenSSL::SSL::VERIFY_NONE
         end
         
-        if options[:tls_key_file]
-          options[:tls_key] = OpenSSL::PKey::RSA.new(File.read(options[:tls_key_file]))
+        if options[:tls_key_and_certificate_file]
+          key_and_certificate       = File.read(options[:tls_key_and_certificate_file])
+          options[:tls_key]         = OpenSSL::PKey::RSA.new(key_and_certificate)
+          options[:tls_certificate] = OpenSSL::X509::Certificate.new(key_and_certificate)
         end
         
-        if options[:tls_key]
-          http_request.key = options[:tls_key]
+        if options[:tls_key] and options[:tls_certificate]
+          http_request.key  = options[:tls_key]
+          http_request.cert = options[:tls_certificate]
+        elsif options[:tls_key] || options[:tls_certificate]
+          raise ArgumentError, "Please specify both the certificate and private key (:tls_key and :tls_certificate)"
         end
       end
       
